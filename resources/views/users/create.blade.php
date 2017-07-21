9372d5b9ca122ddfa543cdad6fc0f738ff472df8
Ren <ren.mendoza06@yahoo.com>
2017-07-21 11:08:08 +0800

    small btn

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -186,7 +186,7 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary btn-sm">
                                     Add User
                                 </button>
                             </div>
